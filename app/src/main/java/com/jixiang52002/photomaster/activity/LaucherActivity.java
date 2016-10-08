fe3e3119827200e5a18116dc6f31f5e500bed429
jixiang52002 <37310534@qq.com>
2016-10-08 09:23:41 +0800

    orinsim3d效果的module导入

---
+++ app/src/main/java/com/jixiang52002/photomaster/activity/LaucherActivity.java
@@ -0,0 +1,23 @@
+package com.jixiang52002.photomaster.activity;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+
+import rk.or.Commands;
+import rk.or.Model;
+import rk.or.android.ModelSelection;
+import rk.or.android.ModelView;
+import rk.or.android.View3D;
+
+public class LaucherActivity extends ModelSelection{
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
---
+++ orinsin3d/src/androidTest/java/wuziqi/jixiang/com/orinsin3d/ApplicationTest.java
@@ -0,0 +1,13 @@
+package wuziqi.jixiang.com.orinsin3d;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
