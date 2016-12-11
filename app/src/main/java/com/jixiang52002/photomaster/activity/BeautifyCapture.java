5e7da7b562a23a53cd2eec0b89d86ec3d3a93445
zn <373105324@qq.com>
2016-12-11 21:30:46 +0800

    项目中所需要使用到的布局资源

--- app/src/main/java/com/jixiang52002/photomaster/activity/BeautifyCapture.java
+++ app/src/main/java/com/jixiang52002/photomaster/activity/BeautifyCapture.java
@@ -1,7 +1,9 @@
 package com.jixiang52002.photomaster.activity;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.jixiang52002.photomaster.R;
 
@@ -10,6 +12,15 @@ public class BeautifyCapture extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+         /*
+         * 防止键盘挡住输入框
+         * 不希望遮挡设置activity属性 android:windowSoftInputMode="adjustPan"
+         * 希望动态调整高度 android:windowSoftInputMode="adjustResize"
+         */
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.
+                SOFT_INPUT_ADJUST_PAN);
+        //锁定屏幕
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_beautify_capture);
     }
 }
